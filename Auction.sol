ce855e51179022c6cfe7b7a065db20ead86a7103
Michael Daigler <mdaigler2601@gmail.com>
2021-04-22 09:47:56 -0400

    Adding state vars to contract

--- Auction.sol
+++ Auction.sol
@@ -1,5 +1,27 @@
 pragma solidity ^0.8.0;
 
 contract Auction {
+    address payable public owner;
+    uint public startBlock;
+    uint public endBlock;
+    string public ipfsHash;
+
+    enum AuctionState {Started, Running, Ended, Canceled }
+
+    AuctionState public auctionState;
+
+    uint public highestBindingBid;
+    address public hihgestBidder;
+    uint public bidIncrement;
+
+    mapping(address => uint) public bids;
+    constructor() {
+        owner = payable(msg.sender);
+        startBlock = block.number;
+        endBlock = block.number + 40230;
+        ipfsHash = "";
+        bidIncrement = 10;
+
+    }
 
 }
